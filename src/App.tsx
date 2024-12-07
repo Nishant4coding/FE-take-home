7fd7e96aafdbb348e750a60e003f5c6c82c702bb
Nishant Srivastava <92370893+Nishant4coding@users.noreply.github.com>
2024-12-07 14:56:42 +0000

    added tailwind css setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App: React.FC = () => {
     return (
         <div>
-            <h1>Frontend Assignment</h1>
+            <h1 className='text-red-200'>Frontend Assignment</h1>
             {/* TODO: Add dropdown and table components here */}
         </div>
     );
